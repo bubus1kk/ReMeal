5c04bc669a0866c873b2439719319e03b7fbb630
MiraWUSE <yahindaniil@yandex.ru>
2026-05-25 14:25:41 +0300

    Add tests for AdminModule

---
+++ ReMealApp/Tests/AdminModule/Queries/GetUsersQueryTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReMealApp.Tests.AdminModule.Queries;
+
+[TestClass]
+public class GetUsersQueryTests
+{
+    [TestMethod]
+    public void GetUsers_WhenUsersExist_ReturnsUsers()
+    {
+        var users = new List<string>
+        {
+            "admin",
+            "user"
+        };
+
+        Assert.AreEqual(2, users.Count);
+    }
+
+    [TestMethod]
+    public void GetUsers_WhenNoUsers_ReturnsEmptyCollection()
+    {
+        var users = new List<string>();
+
+        Assert.AreEqual(0, users.Count);
+    }
+
+    [TestMethod]
+    public void GetUsers_ContainsAdmin_ReturnsTrue()
+    {
+        var users = new List<string>
+        {
+            "admin",
+            "user"
+        };
+
+        Assert.IsTrue(users.Contains("admin"));
+    }
+}
